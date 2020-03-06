3a032d5e6eab2243abe45d5542180b119d8e3efb
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-03-06 17:25:59 +0200

    * switched to private dataset * slug added to documents * cleaned up unused parameters * Translated titles to lithuanian

--- schemas/documentTypes/quiz.js
+++ schemas/documentTypes/quiz.js
@@ -1,32 +1,53 @@
-import multichoiceQuestion from '../schemaTypes/multichoiceQuestion';
 import button from '../schemaTypes/button';
 
 export default {
   name: 'multichoice',
   type: 'document',
-  title: 'Multichoice quiz',
+  title: 'Asteroidų žaidimas',
   fields: [
     {
+      name: 'contentSlug',
+      title: 'Turinio kodas (reikalingas sukurti pamokai)',
+      type: 'slug',
+      options: {
+        source: 'quizTitle',
+        maxLength: 30, // will be ignored if slugify is set
+        slugify: input => input
+          .toLowerCase()
+          .replace(/\s+/g, '-')
+          .slice(0, 15) +
+          '/' + new Date().toISOString().slice(5, 19),
+      },
+      validation: Rule => Rule.required()
+    },
+    {
       name: 'quizTitle',
+      title: 'Testo Pavadinimas',
       type: 'string',
+      validation: Rule => Rule.required()
     },
     {
       name: 'explanation',
+      title: 'Klausimo paaiškinimas',
       type: 'text',
     },
     {
       name: 'correctAnswers',
+      title: 'Teisingi atsakymai',
       type: 'array',
       of: [
         button,
-      ]
+      ],
+      validation: Rule => Rule.required().min(1)
     },
     {
       name: 'incorrectAnswers',
+      title: 'Neteisingi atsakymai',
       type: 'array',
       of: [
         button,
-      ]
+      ],
+      validation: Rule => Rule.required().min(1)
     }
   ]
 };
--- schemas/schemaTypes/button.js
+++ schemas/schemaTypes/button.js
@@ -4,24 +4,15 @@ export default {
   fields: [
     {
       name: 'title',
+      title: 'Atsakymas',
       type: 'string',
     },
-    {
-      name: 'trackable',
-      description: 'Tracks the time of click',
-      type: 'boolean'
-    },
-    {
-      name: 'disappears',
-      description: 'Is removed on click',
-      type: 'boolean'
-    },
-    {
-      name: 'backgroundColor',
-      type: 'string',
-      options: {
-        list: ['white', 'black'],
-      }
-    }
+    // {
+    //   name: 'backgroundColor',
+    //   type: 'string',
+    //   options: {
+    //     list: ['white', 'black'],
+    //   }
+    // }
   ]
 }
